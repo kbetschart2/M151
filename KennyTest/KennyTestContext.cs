fe556ee75d417ad124c075eb5ca529ea2ed7ca82
kbetschart2 <kennybetschart03@gmail.com>
2020-05-18 13:45:36 +0200

    first push

---
+++ KennyTest/KennyTestContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KennyTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KennyTest
+{
+    public class KennyTestContext : DbContext
+    {
+        public KennyTestContext(DbContextOptions<KennyTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+    }
+}
